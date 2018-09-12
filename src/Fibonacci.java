b79ad69b69d1f9ba36806febca64780650656cca
PEDEN Alexander <apeden@ed.ac.uk>
2018-09-12 17:14:23 +0100

    commiting src of Fibonacci

---
+++ src/Fibonacci.java
@@ -0,0 +1,19 @@
+
+public class Fibonacci {
+/*constant variable for class*/
+	MAX_TERM = 10000;
+	fib = 0;
+	term = 0;
+	while (term < 10000);{
+		if(fib == 0);{
+			term = 0;
+		}else if (fib == 1)
+			term = 1
+			else term = += fib		
+	
+		public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
